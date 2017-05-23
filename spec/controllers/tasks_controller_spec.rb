dfa4883e3ee6c98734aaacf8212b14a0ef4d9233
Kristin Heininger <dauphindouze@comcast.net>
2017-05-23 23:48:45 +0000

    Added Update action for tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -10,5 +10,15 @@ RSpec.describe TasksController, type: :controller do
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
     end
+
+    describe 'tasks#update' do
+      it "should allow tasks to be marked as done" do
+        task = FactoryGirl.create(:task, done: false)
+        put :update, params: { id: task.id, task: { done: true } }
+        expect(response).to have_http_status(:success)
+        task.reload
+        expect(task.done).to eq(true)
+      end
+    end
   end
 end
